22db7ab947d8f0c39c7504197ad4933b7cb61afb
Chandan <sinhachandan4992@gmail.com>
2026-03-25 19:54:55 +0530

    added

---
+++ src/main/java/com/demo/trail/Employee.java
@@ -0,0 +1,47 @@
+
+package com.demo.trail;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("Employee")
+public class Employee {
+
+    @Id
+    private int id;
+    private String name;
+    private String city;
+    private String country;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
